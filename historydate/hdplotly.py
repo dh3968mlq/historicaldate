be2bf94dd28f0731c8883bb1c4faedd54921aea8
David Harris <dharris3968@hotmail.co.uk>
2023-05-01 12:38:21 +0100

    First attempt at point events

--- historydate/hdplotly.py
+++ historydate/hdplotly.py
@@ -25,13 +25,27 @@ class plTimeLine():
 
 # ------------------------------------------------------------------------------------------------
 
-def add_trace_part(fig, pdate_start=None, pdate_end=None, label="", y=0.0, color=None, 
+def add_trace_part(fig, pdate_start=None, pdate_end=None, label="", y=0.0, 
+                   ongoing=False, eventful=False, color=None, 
                    width=4, dash=None, showlegend=False, showlabel=False,
                    hovertext=None, tdelta=5*365.25,
                    hyperlink=None):
-    pdate_end_local = pdate_end if pdate_end else pdate_start + datetime.timedelta(days=tdelta)
-    dash_local = dash if pdate_end else 'dot'
-    if (pdate_start < pdate_end_local) or showlegend:
+    if ongoing:
+        pdate_end_local = pdate_start + datetime.timedelta(days=tdelta)
+        dash_local = 'dot'
+    else:
+        pdate_end_local = pdate_end 
+        dash_local = dash 
+
+    if eventful and ((pdate_end_local is None) or (pdate_start == pdate_end_local)):
+        xs = [pdate_start]
+        ys = [y]
+        fig.add_trace(go.Scatter(x = xs, y=ys, name=label, legendgroup=label,
+                            mode="markers", marker={'color':color}, 
+                            hoverinfo='text',
+                            hovertext=hovertext if hovertext else label,
+                            hoverlabel={'namelength':-1}, showlegend=showlegend))
+    elif (pdate_start < pdate_end_local): #or showlegend: # showlegend may be irrelevant
         xs = [pdate_start] + \
              [datetime.date(year, 1, 1) for year in 
                     range(pdate_start.year + 1, pdate_end_local.year + 1)] + [pdate_end_local]
@@ -42,20 +56,21 @@ def add_trace_part(fig, pdate_start=None, pdate_end=None, label="", y=0.0, color
                             hovertext=hovertext if hovertext else label,
                             hoverlabel={'namelength':-1}, showlegend=showlegend))
         
-        if pdate_end is None:     # -- Add 'ongoing' arrow
+        if ongoing:     # -- Add 'ongoing' arrow
             fig.add_trace(go.Scatter(x = [pdate_end_local + datetime.timedelta(days=300)], y=[y], 
                             name=label, legendgroup=label,
                             mode="markers", marker={'color':color,'symbol':'arrow-right','size':12}, 
                             hoverinfo='skip', showlegend=False))
 
-        # Show the label if required
-        if showlabel:
-            hlinkedtext = f'<a href="{hyperlink}">{label}</a>' if hyperlink else label
-            fig.add_trace(go.Scatter(x = [pdate_start + (pdate_end_local - pdate_start)/2.0], y=[y+0.04], # -0.04], 
-                                     name=label, legendgroup=label,
-                            mode="text", text=hlinkedtext, 
-                            textposition='bottom center',
-                            hoverinfo='skip', hoverlabel={'namelength':-1}, showlegend=False))
+    # Show the label if required
+    if showlabel:
+        hlinkedtext = f'<a href="{hyperlink}">{label}</a>' if hyperlink else label
+        fig.add_trace(go.Scatter(x = [pdate_start + (pdate_end_local - pdate_start)/2.0], 
+                                 y=[y+0.04], # -0.04], 
+                                    name=label, legendgroup=label,
+                        mode="text", text=hlinkedtext, 
+                        textposition='bottom center',
+                        hoverinfo='skip', hoverlabel={'namelength':-1}, showlegend=False))
 
 # ------------------------------------------------------------------------------------------------
 def calc_pdates(ahdate):
@@ -65,47 +80,79 @@ def calc_pdates(ahdate):
 def calc_yeartext(pdates):
     return str(pdates['core'].year) + \
                 ("" if pdates['early'].year == pdates['late'].year else "?")
-
 # ------------------------------------------------------------------------------------------------
 
 def add_timeline_trace(fig, row, y=0.0, showbirthanddeath=False, showlegend=True, color=None):
+    pdates_end = calc_pdates(row["hdate_end"])
+    if pdates_end:
+       add_timeline_trace_persistent(fig, row, y=y, 
+                showbirthanddeath=showbirthanddeath, 
+                showlegend=showlegend, color=color)
+    else:
+       add_timeline_trace_event(fig, row, y=y, 
+                showlegend=showlegend, color=color)
+
+# ------------------------------------------------------------------------------------------------
+
+def add_timeline_trace_persistent(fig, row, y=0.0, 
+                                  showbirthanddeath=False, showlegend=True, color=None):
     '''
-    Add a timeline trace for a given row
+    Add a persistent timeline trace for a given row
+    'persistent' is defined by hdate_end is not None
     '''
     
     text = row["label"]
-
-    # -- Main part
     pdates_start = calc_pdates(row["hdate"])
     pdates_end = calc_pdates(row["hdate_end"])
-    if pdates_end:
-        hovertext = f"{text} ({calc_yeartext(pdates_start)}-{calc_yeartext(pdates_end)})"
-    else:
-        hovertext = f"{text} ({calc_yeartext(pdates_start)}...)"
+    ongoing = pdates_end["ongoing"]
 
-    add_trace_part(fig, pdate_start=pdates_start['late'], 
-                   pdate_end=pdates_end['early'] if pdates_end else None, 
-                   label=text, y=y, color=color, showlegend=showlegend, showlabel=True,
-                   hovertext=hovertext, hyperlink=row['wikipedia_url'])
+    if ongoing:
+        hovertext = f"{text} ({calc_yeartext(pdates_start)}...)"
+    else:
+        hovertext = f"{text} ({calc_yeartext(pdates_start)}-{calc_yeartext(pdates_end)})"
+
+    # Main part, from hdate to hdate_end
     add_trace_part(fig, pdate_start=pdates_start['early'], pdate_end=pdates_start['late'], 
                    label=text, y=y, color=color, width=1, hovertext=hovertext)
-    if pdates_end:
+    add_trace_part(fig, pdate_start=pdates_start['late'], 
+                   pdate_end=pdates_end['early'], 
+                   ongoing=ongoing, eventful=True,
+                   label=text, y=y, color=color, showlegend=showlegend, showlabel=True,
+                   hovertext=hovertext, hyperlink=row['wikipedia_url'])
+    if not ongoing:
         add_trace_part(fig, pdate_start=pdates_end['early'], pdate_end=pdates_end['late'], 
                        label=text, y=y, color=color, width=1, hovertext=hovertext)
     
     if showbirthanddeath:
-        pdates_birth = calc_pdates(row["hdate_birth"])
-        hovertext = f"{text} (b. {calc_yeartext(pdates_birth)})"
-        add_trace_part(fig, pdate_start=pdates_birth['late'], 
-                            pdate_end=pdates_start.get('early',None), 
-               label=text, y=y, color=color, dash='dot', hovertext=hovertext)
-        add_trace_part(fig, pdate_start=pdates_birth['early'], pdate_end=pdates_birth['late'], 
-                   label=text, y=y, color=color, width=1, dash='dot', hovertext=hovertext)
+        if pdates_birth := calc_pdates(row["hdate_birth"]):
+            hovertext = f"{text} (b. {calc_yeartext(pdates_birth)})"
+            add_trace_part(fig, pdate_start=pdates_birth['late'], 
+                                pdate_end=pdates_start['early'], 
+                label=text, y=y, color=color, dash='dot', hovertext=hovertext)
+            add_trace_part(fig, pdate_start=pdates_birth['early'], pdate_end=pdates_birth['late'], 
+                    label=text, y=y, color=color, width=1, dash='dot', hovertext=hovertext)
 
-        if pdates_end:
-            pdates_death = calc_pdates(row["hdate_death"])
+        if pdates_death := calc_pdates(row["hdate_death"]):
             hovertext = f"{text} (d. {calc_yeartext(pdates_death)})"
             add_trace_part(fig, pdate_start=pdates_end['late'], pdate_end=pdates_death['early'], 
                    label=text, y=y, color=color, dash='dot', hovertext=hovertext)
             add_trace_part(fig, pdate_start=pdates_death['early'], pdate_end=pdates_death['late'], 
                    label=text, y=y, color=color, width=1, dash='dot', hovertext=hovertext)
+
+def add_timeline_trace_event(fig, row, y=0.0, showlegend=True, color=None):
+    '''
+    Add timeline trace for an event
+    Initial implementation: no births and deaths
+    '''
+    
+    text = row["label"]
+    pdates = calc_pdates(row["hdate"])
+
+    hovertext = f"{text} ({calc_yeartext(pdates)})"
+
+    add_trace_part(fig, pdate_start=pdates['early'], pdate_end=pdates['late'], 
+                   label=text, y=y, color=color, width=1, hovertext=hovertext)
+    add_trace_part(fig, pdate_start=pdates['core'], eventful=True,
+                   label=text, y=y, color=color, showlegend=showlegend, showlabel=True,
+                   hovertext=hovertext, hyperlink=row['wikipedia_url'])
+    
