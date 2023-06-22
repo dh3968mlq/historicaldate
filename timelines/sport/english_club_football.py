0e2df35864c4c14d9db33621c95a2963a2456bc7
David Harris <dharris3968@hotmail.co.uk>
2023-06-22 21:20:56 +0100

    Add FA cup and colour lookups

--- timelines/sport/english_club_football.py
+++ timelines/sport/english_club_football.py
@@ -10,15 +10,31 @@ from historicaldate import hdpl
 import pandas as pd
 import datetime
 
-df_epl = pd.read_csv(f"{dataroot}/data/sport/football/english_premier_league.csv",
-                 na_filter=False)
-df_ucl = pd.read_csv(f"{dataroot}/data/sport/football/uefa_champions_league.csv",
-                 na_filter=False)
+# Abbreviations and colours
+df_colxref = pd.read_csv(f"{dataroot}/data/sport/football/team_abbreviations.csv")[["abbreviation","colour"]]
+df_colxref = df_colxref[df_colxref["colour"].notna()]
+df_colxref["abbreviation"] = df_colxref["abbreviation"].str.strip()
+
+def read_file(filename):
+    "Read file and merge with colours cross-reference"
+    df = pd.read_csv(filename, na_filter=False)
+    if "abbreviation" in df.columns:
+        df["abbreviation"] = df["abbreviation"].str.strip()
+        if "colour" in df.columns:
+            df = df.drop(columns="colour")
+        df = df.merge(df_colxref, on="abbreviation", how="left", validate="m:1")
+        df["colour"] = df["colour"].fillna("black")
+    return df
+
+df_epl = read_file(f"{dataroot}/data/sport/football/english_premier_league.csv")
+df_facup = read_file(f"{dataroot}/data/sport/football/fa_cup.csv")
+df_ucl = read_file(f"{dataroot}/data/sport/football/uefa_champions_league.csv")
 
 pltl = hdpl.plTimeLine(mindate=datetime.date(1990,7,1), 
                        maxdate=datetime.date.today() + datetime.timedelta(days=400),
                        title="English Club Football")
 pltl.add_event_set(df_epl, title="Top Tier: Premier League", hover_datetype='year')
+pltl.add_event_set(df_facup, title="FA Cup")
 pltl.add_event_set(df_ucl, title="UEFA Champions League", hover_datetype='year')
 pltl.show() 
 
